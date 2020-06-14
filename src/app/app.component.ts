296c3e107e6074c74587df4724e2ecfb8b2935e4
liondor <johen.sylvestre@gmail.com>
2020-06-14 11:33:28 -0400

    Fin de la première partie OCR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,16 @@ export class AppComponent {
  appOne ="Machine à laver";
  appTwo =" Fridge";
  appThree ="Ma_yo_nn_ai_se";
+
+ lastUpdate = new Promise((resolve, reject) => {
+   const date = new Date();
+   setTimeout(
+     () => {
+       resolve(date);
+     }, 2000
+   );
+   });
+
   appareils = [
     {
       name: 'Machine à laver',
@@ -25,13 +35,32 @@ export class AppComponent {
     }
   ];
 
-
+  post1 = {
+    title: 'Mon premier post',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut odio dolor, pretium eget sapien et, commodo venenatis ipsum. Sed volutpat mauris et lectus consectetur, quis ullamcorper nulla condimentum.',
+    loveIts: 0,
+    created_at: new Date()
+  }
+  post2= {
+    title: 'Mon deuxième post',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut odio dolor, pretium eget sapien et, commodo venenatis ipsum. Sed volutpat mauris et lectus consectetur, quis ullamcorper nulla condimentum.',
+    loveIts: 0,
+    created_at: new Date()
+  }
+  post3= {
+    title: 'Encore un post',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut odio dolor, pretium eget sapien et, commodo venenatis ipsum. Sed volutpat mauris et lectus consectetur, quis ullamcorper nulla condimentum.',
+    loveIts: 0,
+    created_at: new Date()
+  }
+  listOfPost =[this.post1,this.post2,this.post3];
   constructor(){
    setTimeout(
      () => {
        this.isAuth=true;
      },4000
    );
+   console.log(this.listOfPost)
  }
  onAllumer() {
    console.log('On allume tout !');
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { MonPremierComponent } from './mon-premier/mon-premier.component';
 import { AppareilComponent } from './appareil/appareil.component';
 import {FormsModule} from "@angular/forms";
+import { PostListComponentsComponent } from './post-list-components/post-list-components.component';
+import { PostListItemComponentsComponent } from './post-list-item-components/post-list-item-components.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MonPremierComponent,
-    AppareilComponent
+    AppareilComponent,
+    PostListComponentsComponent,
+    PostListItemComponentsComponent
   ],
   imports: [
     BrowserModule,
